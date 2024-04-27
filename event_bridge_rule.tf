f4a87a28c00e3b1ed3e90a1d53df26ea04a4d2a3
Victor Paulo <victorpaulo.ti@gmail.com>
2024-04-27 14:58:48 -0300

    fix: json valid in event

--- event_bridge_rule.tf
+++ event_bridge_rule.tf
@@ -2,22 +2,16 @@ resource "aws_cloudwatch_event_rule" "rule_chatbot" {
   name        = var.rule_name
   description = "Event send notifications for chatbot"
 
-  event_pattern = jsonencode({
-        "version": "0",
-        "id": "event_id_20240624",
+  event_pattern = jsonencode(
+    {
         "detail-type": "EC2 Instance State-change Notification",
         "source": "aws.ec2",
-        # "account": "730335411417",
-        # "time": "2017-12-22T18:43:48Z",
-        "region": "us-east-1",
-        "resources": [
-            "arn:aws:ec2:us-east-1:730335411417:instance/i-0fac74e47a2d9b9cc"
-        ],
         "detail": {
             "instance-id": "0fac74e47a2d9b9cc",
             "state": "stopped"
         }
-    })
+    }
+    )
 
   tags = var.default_tags
 }
