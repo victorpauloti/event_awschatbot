6e14b4315b65e823cddaaf79b862cd9e76808cd1
Victor Paulo <victorpaulo.ti@gmail.com>
2024-04-27 13:50:40 -0300

    fix: disable account and time in json event pattern

--- event_bridge_rule.tf
+++ event_bridge_rule.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_event_rule" "console" {
+resource "aws_cloudwatch_event_rule" "rule_chatbot" {
   name        = var.rule_name
   description = "Event send notifications for chatbot"
 
@@ -7,8 +7,8 @@ resource "aws_cloudwatch_event_rule" "console" {
         "id": "event_id_20240624",
         "detail-type": "EC2 Instance State-change Notification",
         "source": "aws.ec2",
-        "account": "730335411417",
-        "time": "2017-12-22T18:43:48Z",
+        # "account": "730335411417",
+        # "time": "2017-12-22T18:43:48Z",
         "region": "us-east-1",
         "resources": [
             "arn:aws:ec2:us-east-1:730335411417:instance/i-0fac74e47a2d9b9cc"
