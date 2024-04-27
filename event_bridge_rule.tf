b18ccd9859ce5e3eea2d00e63c5a36f7860a5105
Victor Paulo <victorpaulo.ti@gmail.com>
2024-04-27 18:03:17 -0300

    fix: json minimal

--- event_bridge_rule.tf
+++ event_bridge_rule.tf
@@ -4,12 +4,7 @@ resource "aws_cloudwatch_event_rule" "rule_chatbot" {
 
   event_pattern = jsonencode(
     {
-    "detail": {
-        "instance-id": "0fac74e47a2d9b9cc",
-        "state": "stopped"
-    },
-    "detail-type": "EC2 Instance State-change Notification",
-    "source": "aws.ec2"
+    "detail-type": "EC2 Instance State-change Notification"
     }
     )
 
